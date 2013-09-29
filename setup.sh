a20cf72578ff4c3d494cb88b4f4c6e6b60e03657
Fredrik Sudmann <fsudmann@gmail.com>
2013-09-30 01:20:36 +0200

    Finishing grub config script

--- setup.sh
+++ setup.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 # Simple script for setting up a new Ubuntu Session
 
-cd
+# Create directory and download script- and configfiles.
+cd $HOME
 mkdir scripts
 cd scripts
 git clone https://github.com/fenre/scripts.git
+
+
+# Replace grub config file to get correct console resolution
+sudo cp config/grub /etc/default/grub
+sudo grub-update
 
